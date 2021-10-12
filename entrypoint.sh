9206df1be2d0e2eab2641e046582b6ee254f2d5e
Antonina A <aarriagada@realit.cl>
2021-10-12 11:22:26 -0300

    refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,10 +30,11 @@ branch_uri="$(urlencode ${branch})"
 
 sh -c 'echo iniciando proceso de sshkey'
 sh -c "mkdir -p ~/.ssh/"
-sh -c "echo \"$SSH_PRIVATE_KEY\" > ../gitlab_bot"
-sh -c 'chmod 600 ../gitlab_bot'
-sh -c 'echo terminando proceso de sshkey'
-
+sh -c "ls"
+sh -c "echo '$SSH_PRIVATE_KEY' > ~/.ssh/gitlab_bot"
+sh -c 'chmod 600 ~/.ssh/gitlab_bot'
+sh -c '[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config'
+sh -c 'echo ending proceso de sshkey'
 
 sh -c "echo $*"
 sh -c "git config --global credential.username $GITLAB_USERNAME"
